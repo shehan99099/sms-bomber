b0ddd60c80a16b86fdd169c0d90ab9f03c892705
shehan99099 <62324980+shehan99099@users.noreply.github.com>
2020-06-19 12:56:42 +0530

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -1,40 +1,70 @@
-#-------------------------------------------------------------------------------
-# Name:        Way2sms
-# Purpose:   send sms way2sms #
-# Author:      Rishabh Roy
-# Created:     09/08/2014
-#-------------------------------------------------------------------------------
-import urllib2
+
+#!/usr/bin/python
+__author__ = """
+NAME: Abhijeet Rastogi (shadyabhi)
+Profile: http://www.google.com/profiles/abhijeet.1989
+"""
 import cookielib
+import urllib2
+from getpass import getpass
 import sys
-username=  ""    #write ur mobile number for way2sms inside " " 
-passwd=   ""       #password for way2sms inside " "
-message=raw_input("messge u want to send ")
-number=raw_input("number of person u want to send sms ")
-message="+".join(message.split(' '))
-url= 'Send Free SMS to any mobile in india'
-data = 'username='+username+'&password='+passwd
-cj =cookielib.CookieJar()
-opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-opener.addheaders=[('User-Agent',"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36")]
+from urllib import urlencode
+from getopt import getopt
+username = None
+passwd = None
+message = None
+number = None
+def Usage():
+print '\t-h, --help:  View help'
+print '\t-u, --username: Username'
+print '\t-p, --password: Password'
+print '\t-n, --number: numbber to send the sms'
+print '\t-m, --message: Message to send'
+sys.exit(1)
+opts, args = getopt(sys.argv[1:], 'u:p:m:n:h',["username=","password=","message=","number=","help"])
+for o,v in opts:
+if o in ("-h", "--help"):
+Usage()
+elif o in ("-u", "--username"):
+username = v
+ask_username = False
+elif o in ("-p", "--password"):
+passwd = v
+ask_password = False
+elif o in ("-m", "--message"):
+message = v
+ask_message = False
+elif o in ("-n", "--number"):
+number = v
+ask_number = False
+#Credentials taken here
+if username is None: username = raw_input("Enter USERNAME: ")
+if passwd is None: passwd = getpass()
+if message is None: message = raw_input("Enter Message: ")
+if number is None: number = raw_input("Enter Mobile number: ")
+#Logging into the SMS Site
+url = 'http://wwwb.way2sms.com//auth.cl'
+data = 'username='+username+'&password='+passwd+'&Submit=Sign+in'
+#Remember, Cookies are to be handled
+cj = cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+# To fool way2sms as if a Web browser is visiting the site
+opener.addheaders = [('User-Agent','Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20091020 Ubuntu/9.10 (karmic) Firefox/3.5.3 GTB7.0')]
 try:
-        usock = opener.open(url,data)
+usock = opener.open(url, data)
 except IOError:
-    print "cannot connect "
-    sys.exit(1)
-jession_id=str(cj).split('~')[1].split(' ')[0]
-print jession_id
-p= opener.open("http://site21.way2sms.com/ebrdg.action?id="+jession_id)
-send_sms_url='http://site21.way2sms.com/smstoss.action'
-send_sms_data= 'ssaction=ss&Token='+jession_id+'&mobile='+number+'&message='+message+'&msgLen='+str(140-len(message))
-opener.addheaders=[('Referer','http://site21.way2sms.com/sendSms?Token='+jession_id)]
+print "Check your internet connection"
+sys.exit(1)
+#urlencode performed.. Because it was done by the site as i checked through HTTP headers
+message = urlencode({'message':message})
+message = message[message.find("=")+1:]
+#SMS sending
+send_sms_url = 'http://wwwb.way2sms.com//FirstServletsms?custid='
+send_sms_data = 'custid=undefined&HiddenAction=instantsms&Action=custfrom950000&login=&pass=&MobNo='+number+'&textArea='+message
+opener.addheaders = [('Referer','http://wwwb.way2sms.com//jsp/InstantSMS.jsp?val=0')]
 try:
-    sms_sent_page = opener.open(send_sms_url,send_sms_data)
-except IOError as e :
-    print e
-p=opener.open('http://site21.way2sms.com/smscofirm.action?SentMessage='+message+'&Token='+jession_id+'&status=0')
-
-
-
-
-    
+sms_sent_page = opener.open(send_sms_url,send_sms_data)
+except IOError:
+print "Check your internet connection( while sending sms)"
+sys.exit(1)
+print "SMS sent!!!
