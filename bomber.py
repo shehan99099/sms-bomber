a1a6d1e0f59de5ef71e16ab7aac34d8f7091f874
shehan99099 <62324980+shehan99099@users.noreply.github.com>
2020-06-19 10:14:47 +0530

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -22,7 +22,7 @@ print("\033[0;36m ""      //  //        //              //                    //
 print("\033[0;36m "" /////// //        //         ////////                    //        //////////  //////////  //////////")
 print("\033[0;35m ""                                          [TOOL BY MEGARUN] ")
 print("")
-print("\033[1;33m ""#YOU CAN SEND 10 MESSAGE ONLY ONE NUMBER (one days)")
+print("\033[1;33m ""#YOU CAN SEND 10 MESSAGE ONLY ONE NUMBER")
 print("\033[0;32m ""#OUT PUT = 200  message was sent successfully")
 print("\033[1;31m ""#OUT PUT = 400  LIMITED ")
 print("")
@@ -38,42 +38,36 @@ def main():
 
 
 
-    url = "https://getshoutout.com/contactus"
+url : 'https://api.getshoutout.com/coreservice/messages',
 
-    headers = {
-    "accept": "application/json, text/javascript, */*; q=0.01",
-    "accept-encoding": "gzip, deflate, br",
-    "accept-language": "en-US,en;q=0.9,si;q=0.8",
-    "cache-control": "no-cache",
-    "content-length":"81",
-    "content-type":  "application/json",
-    "device-memory": "4",
-    "dpr": "1",
-    "ect": "4g",
-    "origin": "https://getshoutout.com/",
-    "referer": "https://getshoutout.com/",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36",
-    "viewport-width": "811",
-    "x-csrf-token": "V4$.airbnb.com$2b6lgizn2c8$_YXxw_npUQGCUwtRNBIIGBa7dhCZoLqPs7ewu6tF-YU=",
-    "x-csrf-without-token": "1",
-    "x-requested-with": "XMLHttpRequest",
+method : 'POST',
 
-    }    
+headers : {
 
-    params = {
-    "currency": "USD",
-    "key": "d306zoyjsyarp7ifhu67rjxn52tv0t20",
-    "locale": "en",
+'Content-Type' : 'application/json',
+
+'Authorization' : 'Apikey xxxxx.xx.xx.xxxx',
+
+},
+
+body : JSON.stringify({
+
+"source" : "ShoutDEMO",
+
+"destinations" : ["number"],
+
+"transports" : ["sms"],
+
+"content" : {
+
+"sms" : "Sent via ShoutOUT Lite"
+
+},
+
+}),
+
+
     
-    }    
-    payload = {
-    "otpMethod": "AUTO",
-    "phoneNumber": number,
-    "workFlow": "GLOBAL_SIGNUP_LOGIN",
-    }
 
     ss = 0
     while s > ss:
@@ -84,8 +78,8 @@ def main():
         resp = str(r)
         if resp == '<Response [200]>':
             print("200 success")
-        elif resp == '<Response [400]>':
-            print("400  limited")
+        elif resp == '<Response [204]>':
+            print("204  limited")
         else:
             print("\033[1;31m ""something wrong please try again")
 
@@ -93,14 +87,14 @@ def main():
         print("\033[1;0;40m\n",str(ss), end="")
         for i in range(180):
             
-            pr = i/180*100
+            pr = i/180*1000
             print("\033[1;36;40m\n>>>",str(int(pr)) +"% ",end="")
             
             time.sleep(0.002)
             sys.stdout.write("\033[F")
 
 
-    os.system('figlet FINISHED')
+    os.system('')
     again()
 
 
